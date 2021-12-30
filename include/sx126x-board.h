c458a0bc57bb63e1624bea5e4fdad568f1a4efa1
Lee Lup Yuen <luppy@appkaki.com>
2021-12-30 09:57:54 +0800

    Clean up include

--- include/sx126x-board.h
+++ include/sx126x-board.h
@@ -30,7 +30,7 @@ extern "C"
 
 #include <stdint.h>
 #include <stdbool.h>
-#include "../../libnpl/nimble/include/nimble/nimble_npl.h"  //  For NimBLE Porting Layer (timer functions)
+#include "../libs/libnpl/nimble/include/nimble/nimble_npl.h"  //  For NimBLE Porting Layer (timer functions)
 #include "sx126x.h"
 
 #define SX126X_TCXO_WAKEUP_TIME  5  //  Time required for the TCXO to wakeup (milliseconds)
